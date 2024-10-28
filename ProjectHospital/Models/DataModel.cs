86b6f966b56b80e125a3cdd2064f80dbf3eb4682
VÕ THANH HẢI <22dh114515@st.huflit.edu.vn>
2024-10-28 20:35:01 +0700

    đã thêm giao diện ĐĂNG NHẬP VÀ ĐĂNG KÝ VÀ KẾT NỐI DATAMODEL VỚI SERVER

---
+++ ProjectHospital/Models/DataModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ProjectHospital.Models
+{
+	public class DataModel
+	{
+		private string connectionString = "workstation id=ProjectHospital.mssql.somee.com;packet size=4096;user id=ProjectHospital_SQLLogin_1;pwd=5esvvfzqhi;data source=ProjectHospital.mssql.somee.com;persist security info=False;initial catalog=ProjectHospital;TrustServerCertificate=True";
+
+		public ArrayList get(String sql)
+		{
+			ArrayList dataList = new ArrayList();
+			SqlConnection connection = new SqlConnection(connectionString);
+			SqlCommand commnad = new SqlCommand(sql, connection);
+
+			connection.Open();
+
+
+			using (SqlDataReader r = commnad.ExecuteReader())
+			{
+				while (r.Read())
+				{
+					ArrayList row = new ArrayList();
+					for (int i = 0; i < r.FieldCount; i++) { 
+						row.Add(r.GetValue(i).ToString());
+					}
+					dataList.Add(row);
+				}
+			}
+
+			connection.Close();
+			return dataList;
+		}
+	}
+}
