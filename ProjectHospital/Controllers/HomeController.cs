8a564b17d99f2ed5a395808ded62ae1ce83f1a03
VÕ THANH HẢI <22dh114515@st.huflit.edu.vn>
2024-10-30 08:07:00 +0700

    hoàn thiện chức năng đăng ký và đăng nhập

--- ProjectHospital/Controllers/HomeController.cs
+++ ProjectHospital/Controllers/HomeController.cs
@@ -29,16 +29,42 @@ namespace ProjectHospital.Controllers
 		public ActionResult xuLyDangKychobenhNhan(string HoVaTen,
 										string email,
 										string matkhau
-										) { 
+										)
+		{
+			////tạo biến để lưu giá trị
 			DataModel db = new DataModel();
 			ViewBag.list = db.get("Exec THEMTAIKHOANBenhNhan N'" + HoVaTen + "','" + email + "','" + matkhau + "'");
-			if (ViewBag.list.Count > 0)
+			if (ViewBag.list.Count > 0 && ViewBag.list != null)
 			{
-				Session["taikhoan"] = ViewBag.list[0];
-				return RedirectToAction("GiaoDienDangKy", "Home");
+				//Session["taikhoan"] = ViewBag.list[0];
+				return RedirectToAction("GiaoDienDangNhap", "Home");
 			}
 			else
 			{
+				// Sử dụng TempData để lưu thông báo lỗi
+				TempData["ErrorMessage"] = "Đăng ký thất bại, hãy thử lại.";
+				return RedirectToAction("GiaoDienDangKy", "Home");
+			}
+
+		}
+		
+
+		[HttpPost]
+		public ActionResult xuLyDangNhapchobenhNhan(string email,
+										string matkhau
+										)
+		{
+			DataModel db = new DataModel();
+			ViewBag.list = db.get("Exec DangNhapTaiKhoanbenhNhan '"+email+"','"+matkhau+"'");
+			if (ViewBag.list.Count > 0)
+			{
+				Session["taikhoan"] = ViewBag.list[0];
+				return RedirectToAction("Index", "Home");
+			}
+			else
+			{
+				// Sử dụng TempData để lưu thông báo lỗi
+				TempData["ErrorMessage"] = "Mật khẩu sai hoặc tài khoản không tồn tại.";
 				return RedirectToAction("GiaoDienDangNhap", "Home");
 			}
 		}
