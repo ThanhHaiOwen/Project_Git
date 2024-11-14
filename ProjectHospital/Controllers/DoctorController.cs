3112f82fa78985fa6f72453ee6bda0cc3d0574fb
Đặng Trọng Hoàng Anh <22dh110080@st.huflit.edu.vn>
2024-11-14 23:22:28 +0700

    HienThiBS-TimKiemBS

---
+++ ProjectHospital/Controllers/DoctorController.cs
@@ -0,0 +1,38 @@
+﻿using ProjectHospital.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectHospital.Controllers
+{
+    public class DoctorController : Controller
+    {
+        // GET: Docters
+        public ActionResult Index()
+        {
+
+            DataModel db = new DataModel();
+            ViewBag.listBS = db.get("EXEC HienThiBacSi");
+            return View();
+        }
+        public ActionResult TimKiemBacSi(string hoten)
+        {
+            DataModel db = new DataModel();
+            ViewBag.listBS = db.get("EXEC TimBacSi N'" + hoten + "'");
+
+            if (ViewBag.listBS == null ||   ViewBag.listBS.Count == 0)
+            {
+                ViewBag.Message = "Không tìm thấy bác sĩ nào phù hợp với tên tìm kiếm.";
+            }
+            else
+            {
+                ViewBag.listBS =   ViewBag.listBS;
+            }
+
+            return View();
+        }
+
+    }
+}
